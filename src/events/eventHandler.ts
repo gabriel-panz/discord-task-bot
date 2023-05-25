8e90558a5398020809c8b88b67cd7da933f292de
unknown <gabriel.panzenhagen@gmail.com>
2023-05-24 21:56:39 -0300

    express health check

--- src/events/eventHandler.ts
+++ src/events/eventHandler.ts
@@ -1,15 +1,12 @@
 import { Client } from "discord.js";
-import { commands } from "../commands/slashCommands";
 import ready from "./ready";
 import interactionCreate from "./interactionCreate";
 import TasksRepository from "../repositories/tasksRepository";
-import { TaskDataContext } from "../interfaces/dataContext";
+import express from 'express'
+
+const restApi = express()
 
 const eventHandler = (client: Client<boolean>) => {
-  const dataContext: TaskDataContext = {
-    users: [],
-  };
-
   const repository = new TasksRepository();
 
   client.on("ready", ready);
@@ -17,6 +14,13 @@ const eventHandler = (client: Client<boolean>) => {
   client.on("interactionCreate", (i) => interactionCreate(i, repository));
 
   client.login(process.env.TOKEN);
+
+  restApi.get('/health', (req, res) => res.send(200))
+  
+  let port = process.env.PORT ? Number(process.env.PORT) : 8000
+  restApi.listen(port, () => {
+    console.log(`listening on port: ${port}`)
+  })
 };
 
 export default eventHandler;
