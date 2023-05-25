51ecf9176d85ba5216a1f35a80cad7cd64cf0102
unknown <gabriel.panzenhagen@gmail.com>
2023-05-24 22:57:11 -0300

    removed health check

--- src/events/eventHandler.ts
+++ src/events/eventHandler.ts
@@ -2,9 +2,7 @@ import { Client } from "discord.js";
 import ready from "./ready";
 import interactionCreate from "./interactionCreate";
 import TasksRepository from "../repositories/tasksRepository";
-import express from 'express'
 
-const restApi = express()
 
 const eventHandler = (client: Client<boolean>) => {
   const repository = new TasksRepository();
@@ -14,13 +12,6 @@ const eventHandler = (client: Client<boolean>) => {
   client.on("interactionCreate", (i) => interactionCreate(i, repository));
 
   client.login(process.env.TOKEN);
-
-  restApi.get('/health', (req, res) => res.send(200))
-  
-  let port = process.env.PORT ? Number(process.env.PORT) : 8000
-  restApi.listen(port, () => {
-    console.log(`listening on port: ${port}`)
-  })
 };
 
 export default eventHandler;
